eb27dab796333537cc4af49188ce69eb51cd257a
kristinadarroch <dar15003@byui.edu>
2018-12-10 19:50:49 -0700

    created components

--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherCardComponent } from './cards/weather-card/weather-card.component';
+import { AddCardComponent } from './cards/add-card/add-card.component';
+import { DetailsComponent } from './cards/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
